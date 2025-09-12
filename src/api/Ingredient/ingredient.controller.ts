1654453516b769c933887d9c30c69582c5e5be4b
jello2025 <albarjas2025@gmail.com>
2025-09-12 13:46:42 +0300

    ingredient CRUD. not tested yet

--- src/api/Ingredient/ingredient.controller.ts
+++ src/api/Ingredient/ingredient.controller.ts
@@ -7,6 +7,8 @@ export const getAllIngredients = async (
   next: NextFunction
 ) => {
   try {
+    const ingredients = await Ingredient.find().populate("recipe");
+    res.status(200).json(ingredients);
   } catch (err) {
     next(err);
   }
@@ -17,7 +19,16 @@ export const getIngredientById = async (
   res: Response,
   next: NextFunction
 ) => {
+  const { ingredientId } = req.params;
   try {
+    const foundIngredient = await Ingredient.findById(ingredientId).populate(
+      "recipe"
+    );
+    if (foundIngredient) {
+      return res.status(200).json(foundIngredient);
+    } else {
+      return res.status(404).json({ message: "couldnt find ingredient" });
+    }
   } catch (err) {
     next(err);
   }
@@ -29,6 +40,8 @@ export const createIngredient = async (
   next: NextFunction
 ) => {
   try {
+    const newIngredient = await Ingredient.create(req.body);
+    res.status(201).json(newIngredient);
   } catch (err) {
     next(err);
   }
@@ -39,7 +52,15 @@ export const updateIngredientById = async (
   res: Response,
   next: NextFunction
 ) => {
+  const { ingredientId } = req.params;
   try {
+    const foundIngredient = await Ingredient.findById(ingredientId);
+    if (foundIngredient) {
+      await Ingredient.updateOne(req.body);
+      res.status(204).end();
+    } else {
+      return res.status(404).json({ message: "couldnt find ingredient" });
+    }
   } catch (err) {
     next(err);
   }
@@ -50,7 +71,15 @@ export const deleteIngredientById = async (
   res: Response,
   next: NextFunction
 ) => {
+  const { ingredientId } = req.params;
   try {
+    const foundIngredient = await Ingredient.findById(ingredientId);
+    if (foundIngredient) {
+      await Ingredient.deleteOne();
+      res.status(204).end();
+    } else {
+      return res.status(404).json({ message: "couldnt find ingredient" });
+    }
   } catch (err) {
     next(err);
   }
