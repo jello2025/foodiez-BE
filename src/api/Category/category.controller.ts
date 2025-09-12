5060883b2a6192f776a936b813893159a88ae29d
jello2025 <albarjas2025@gmail.com>
2025-09-12 13:03:17 +0300

    CRUD for category. not tested yet tho

---
+++ src/api/Category/category.controller.ts
@@ -0,0 +1,84 @@
+import { Request, Response, NextFunction } from "express";
+import Category from "../../models/Category";
+
+export const getAllCategories = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const categories = await Category.find();
+    res.status(200).json(categories);
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const getCategoryById = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const { categoryId } = req.params;
+  try {
+    const foundCategory = await Category.findById(categoryId);
+    if (foundCategory) {
+      return res.status(200).json(foundCategory);
+    } else {
+      return res.status(404).json({ message: "category not found" });
+    }
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const createCategory = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const newCategory = await Category.create(req.body);
+    res.status(201).json(newCategory);
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const updateCategoryById = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const { categoryId } = req.params;
+  try {
+    const foundCategory = await Category.findById(categoryId);
+    if (foundCategory) {
+      await foundCategory.updateOne(req.body);
+      res.status(204).end();
+    } else {
+      res.status(404).json({ message: "category not found" });
+    }
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const deleteCategoryById = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const { categoryId } = req.params;
+  try {
+    const foundCategory = await Category.findById(categoryId);
+    if (foundCategory) {
+      await foundCategory.deleteOne();
+      res.status(204).end();
+    } else {
+      res.status(404).json({ message: "category not found" });
+    }
+  } catch (err) {
+    next(err);
+  }
+};
---
+++ src/api/Category/category.routes.ts
@@ -0,0 +1,16 @@
+import { Router } from "express";
+import {
+  getAllCategories,
+  getCategoryById,
+  updateCategoryById,
+  deleteCategoryById,
+  createCategory,
+} from "./category.controller";
+
+const categoryRouter = Router();
+
+categoryRouter.get("/", getAllCategories);
+categoryRouter.get("/:categoryId", getCategoryById);
+categoryRouter.put("/:categoryId", updateCategoryById);
+categoryRouter.post("/", createCategory);
+categoryRouter.delete("/:categoryId", deleteCategoryById);
